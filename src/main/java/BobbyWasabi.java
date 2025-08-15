4d905565f2ef40a6083a5ceec2c69fd1292e4deb
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-08-15 11:58:51 +0800

    Level 2 completed

--- src/main/java/BobbyWasabi.java
+++ src/main/java/BobbyWasabi.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BobbyWasabi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        String decoLine = "____________________________________________________________";
 
         String botGreet = """
                 ____________________________________________________________
@@ -15,16 +19,41 @@ public class BobbyWasabi {
         System.out.println(botGreet);
 
         while (true) {
+
+            // Get user intput
             String userInput = scanner.nextLine();
 
             if (userInput.equals("bye")) {
                 break;
             }
 
+            // if the user asks for list
+            if (userInput.equals("list")) {
+                String textList = "";
+
+                for (int i = 0; i < list.size(); i++) {
+                    String cur = list.get(i);
+                    String curItem = String.format("%d. %s\n", i, cur);
+                    textList += curItem;
+                }
+
+
+                String output = decoLine + "\n" + textList + decoLine;
+
+                System.out.println(output);
+
+                continue;
+            }
+
+            // Store user input
+            list.add(userInput);
+
+
+            // Bot output
             String botOutput = String.format(
             """
             ____________________________________________________________
-            %s
+            added: %s
             ____________________________________________________________
                 
             """, userInput);
