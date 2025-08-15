2119d24ddd8ba916d1091f6ad5abd5992233a0ab
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-08-15 21:08:23 +0800

    Added Event Class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+public class Event extends Task {
+    private String start;
+    private String end;
 
-public class Event {
+    public Event(String description, boolean isMarked, String duration) {
+        super(description, isMarked);
+        this.formatDuration(duration);
+    }
+
+    public void formatDuration(String duration) {
+        String[] durationList = duration.split("/to");
+        this.start = durationList[0];
+        this.end = durationList[1];
+    }
+
+    public String getDuration() {
+        return String.format("(from:%sto:%s",
+                this.start,
+                this.end);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString() + " " + this.getDuration();
+    }
 }
