9062069b8c4d9e0fcf0fe60fa5f7083ab7c87133
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-08-15 11:36:14 +0800

    initial testing of level1

--- src/main/java/BobbyWasabi.java
+++ src/main/java/BobbyWasabi.java
@@ -1,14 +1,45 @@
+import java.util.Scanner;
+
 public class BobbyWasabi {
     public static void main(String[] args) {
-        String text = """
+        Scanner scanner = new Scanner(System.in);
+
+        String botGreet = """
                 ____________________________________________________________
                  Hello! I'm Bobby Wasabi
                  What can I do for you?
                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ___________________________________________________________
                 
                 """;
-        System.out.println(text);
+
+        System.out.println(botGreet);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput == "bye") {
+                break;
+            }
+
+            String botOutput = String.format(
+            """
+            ____________________________________________________________
+            %s
+            ____________________________________________________________
+                
+            """, userInput);
+
+            System.out.println(botOutput);
+
+        }
+
+        System.out.println("""
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+               
+                """);
+
+        scanner.close();
     }
 }
